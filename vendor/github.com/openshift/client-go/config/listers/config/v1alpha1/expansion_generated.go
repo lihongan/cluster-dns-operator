48966e133bdab939f47ce9a6b31959cd7ac0173d
Davide Salerno <dsalerno@redhat.com>
2025-09-12 15:55:38 +0200

    NE-2138: Bump cluster-dns-operator to Kubernetes 1.33 for 4.21 - vendor
    Signed-off-by: Davide Salerno <dsalerno@redhat.com>

--- vendor/github.com/openshift/client-go/config/listers/config/v1alpha1/expansion_generated.go
+++ vendor/github.com/openshift/client-go/config/listers/config/v1alpha1/expansion_generated.go
@@ -6,6 +6,22 @@ package v1alpha1
 // BackupLister.
 type BackupListerExpansion interface{}
 
+// ClusterImagePolicyListerExpansion allows custom methods to be added to
+// ClusterImagePolicyLister.
+type ClusterImagePolicyListerExpansion interface{}
+
+// ClusterMonitoringListerExpansion allows custom methods to be added to
+// ClusterMonitoringLister.
+type ClusterMonitoringListerExpansion interface{}
+
+// ImagePolicyListerExpansion allows custom methods to be added to
+// ImagePolicyLister.
+type ImagePolicyListerExpansion interface{}
+
+// ImagePolicyNamespaceListerExpansion allows custom methods to be added to
+// ImagePolicyNamespaceLister.
+type ImagePolicyNamespaceListerExpansion interface{}
+
 // InsightsDataGatherListerExpansion allows custom methods to be added to
 // InsightsDataGatherLister.
 type InsightsDataGatherListerExpansion interface{}
